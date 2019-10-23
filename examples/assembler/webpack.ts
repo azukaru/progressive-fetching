e98f03415a24efaf4b75c63344a9e87859ffe7d5
Jan Krems <jankrems@google.com>
2019-10-23 14:59:43 -0700

    Split webpack-batching example from assembler

---
+++ examples/assembler/webpack.ts
@@ -0,0 +1,43 @@
+import webpack from 'webpack';
+
+import BatchDynamicPlugin from './batch-dynamic-plugin';
+import {serveBundle} from './middleware';
+
+/**
+* Take "normal" webpack config that doesn't generate multiple initial requests,
+* adds settings that make it work for dynamic bundling.
+*
+* This assumes that the app will try to load `foo.js` for the entrypoint `foo`.
+*/
+export function makeFinegrained(config: webpack.Configuration): webpack.Configuration {
+  config.optimization = config.optimization || {};
+  config.optimization.splitChunks = config.optimization.splitChunks || {};
+
+  Object.assign(config.optimization.splitChunks, {
+    chunks: 'all',
+    minSize: 1,
+    maxAsyncRequests: 100000,
+    maxInitialRequests: 100000,
+  });
+  // Force numeric chunk ids
+  if (!config.optimization.chunkIds || config.optimization.chunkIds === 'named') {
+    config.optimization.chunkIds = 'natural';
+  }
+
+  config.output = config.output || {};
+  config.output.chunkFilename = 'chunk.[id].js';
+  config.output.filename = 'chunk.[id].js';
+
+  config.plugins = config.plugins || [];
+  config.plugins.push(new BatchDynamicPlugin());
+
+  config.devServer = config.devServer || {};
+  const previousAfter = config.devServer.after;
+  config.devServer.after = (app, server) => {
+    if (previousAfter) previousAfter(app, server);
+    const {compiler} = server as unknown as {compiler: webpack.Compiler};
+    app.use(serveBundle(compiler));
+  };
+
+  return config;
+}
--- examples/webpack-batching/webpack.config.ts
+++ examples/webpack-batching/webpack.config.ts
@@ -2,8 +2,7 @@ import path from 'path';
 
 import webpack from 'webpack';
 
-import {serveBundle} from './assembler/middleware';
-import BatchDynamicPlugin from './assembler/batch-dynamic-plugin';
+import {makeFinegrained} from '../assembler/webpack';
 
 const config: webpack.Configuration = {
   context: __dirname,
@@ -23,44 +22,5 @@ const config: webpack.Configuration = {
     contentBase: path.resolve(__dirname, 'static'),
   },
 };
-
-/**
- * Take "normal" webpack config that doesn't generate multiple initial requests,
- * adds settings that make it work for dynamic bundling.
- *
- * This assumes that the app will try to load `foo.js` for the entrypoint `foo`.
- */
-function makeFinegrained(config: webpack.Configuration): webpack.Configuration {
-  config.optimization = config.optimization || {};
-  config.optimization.splitChunks = config.optimization.splitChunks || {};
-
-  Object.assign(config.optimization.splitChunks, {
-    chunks: 'all',
-    minSize: 1,
-    maxAsyncRequests: 100000,
-    maxInitialRequests: 100000,
-  });
-  // Force numeric chunk ids
-  if (!config.optimization.chunkIds || config.optimization.chunkIds === 'named') {
-    config.optimization.chunkIds = 'natural';
-  }
-
-  config.output = config.output || {};
-  config.output.chunkFilename = 'chunk.[id].js';
-  config.output.filename = 'chunk.[id].js';
-
-  config.plugins = config.plugins || [];
-  config.plugins.push(new BatchDynamicPlugin());
-
-  config.devServer = config.devServer || {};
-  const previousAfter = config.devServer.after;
-  config.devServer.after = (app, server) => {
-    if (previousAfter) previousAfter(app, server);
-    const {compiler} = server as unknown as {compiler: webpack.Compiler};
-    app.use(serveBundle(compiler));
-  };
-
-  return config;
-}
 
 export default makeFinegrained(config);
