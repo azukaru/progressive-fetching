fb866ef70b6d615d1000ada4cde03cbe31e90527
Jan Krems <jankrems@google.com>
2019-11-05 11:41:30 -0800

    Make prefix configurable

--- packages/next-dynamic-bundle/src/index.ts
+++ packages/next-dynamic-bundle/src/index.ts
@@ -8,66 +8,72 @@ interface NextConfig {
   webpack?(config: any, options: any);
 }
 
-function buildChunkUrl(prefix, chunkIds) {
-  return `/api/chunks/js/i=${chunkIds.join(',')}`;
+interface DynamicBundleOptions {
+  prefix: string;
 }
 
-export function withDynamicBundle(nextConfig: NextConfig = {}) {
-  return Object.assign({}, nextConfig, {
-    webpack(config, options) {
-      const {dev, isServer} = options;
+export function withDynamicBundle({prefix}: DynamicBundleOptions) {
+  function buildChunkUrl(webpackPrefix, chunkIds) {
+    return `${prefix}/js/i=${chunkIds.join(',')}`;
+  }
 
-      // TODO: Handle DLL plugin when building either the chunk list or the deps
-      // inside of the chunkset.
-      config.plugins = config.plugins.filter(plugin => {
-        const {name} = plugin.constructor;
-        return name !== 'AutoDLLPlugin';
-      });
+  return function applyDynamicBundle(nextConfig: NextConfig = {}) {
+    return Object.assign({}, nextConfig, {
+      webpack(config, options) {
+        const {dev, isServer} = options;
 
-      if (isServer) {
-        // TODO: Figure out if we can add an API route dynamically without
-        //       a dedicated file in the application itself.
-        // const originalEntry = config.entry;
-        // config.entry = async () => {
-        //   const entry = typeof originalEntry === 'function' ?
-        //     await originalEntry() : originalEntry;
-        //   entry[`static/${options.buildId}/pages/api/dca.js`] =
-        //     require.resolve('./chunk-batching-handler');
-        //   return entry;
-        // };
-      } else {
-        config.plugins.push(new DynamicBundlePlugin(buildChunkUrl));
+        // TODO: Handle DLL plugin when building either the chunk list or the deps
+        // inside of the chunkset.
+        config.plugins = config.plugins.filter(plugin => {
+          const {name} = plugin.constructor;
+          return name !== 'AutoDLLPlugin';
+        });
 
-        if (dev) {
-          config.output.chunkFilename = 'static/chunks/[id].js';
+        if (isServer) {
+          // TODO: Figure out if we can add an API route dynamically without
+          //       a dedicated file in the application itself.
+          // const originalEntry = config.entry;
+          // config.entry = async () => {
+          //   const entry = typeof originalEntry === 'function' ?
+          //     await originalEntry() : originalEntry;
+          //   entry[`static/${options.buildId}/pages/api/dca.js`] =
+          //     require.resolve('./chunk-batching-handler');
+          //   return entry;
+          // };
         } else {
-          config.output.chunkFilename = 'static/chunks/[id].[contenthash].js';
+          config.plugins.push(new DynamicBundlePlugin(buildChunkUrl));
 
-          // Object.assign(config.optimization.splitChunks, {
-          //   // Disable built-in next.js cache groups (e.g. commons)
-          //   cacheGroups: {
-          //     default: {
-          //       chunks: 'all',
-          //       minSize: 1,
-          //       name: false,
-          //       maxAsyncRequests: 100000,
-          //       // TODO: Things start breaking once this goes over 5 which smells
-          //       // like a fundamental issue is hiding behind this symptom.
-          //       maxInitialRequests: 5,
-          //       reuseExistingChunk: true,
-          //       enforce: true,
-          //     },
-          //     vendors: false,
-          //   },
-          // });
+          if (dev) {
+            config.output.chunkFilename = 'static/chunks/[id].js';
+          } else {
+            config.output.chunkFilename = 'static/chunks/[id].[contenthash].js';
+
+            // Object.assign(config.optimization.splitChunks, {
+            //   // Disable built-in next.js cache groups (e.g. commons)
+            //   cacheGroups: {
+            //     default: {
+            //       chunks: 'all',
+            //       minSize: 1,
+            //       name: false,
+            //       maxAsyncRequests: 100000,
+            //       // TODO: Things start breaking once this goes over 5 which smells
+            //       // like a fundamental issue is hiding behind this symptom.
+            //       maxInitialRequests: 5,
+            //       reuseExistingChunk: true,
+            //       enforce: true,
+            //     },
+            //     vendors: false,
+            //   },
+            // });
+          }
         }
-      }
 
-      if (typeof nextConfig.webpack === 'function') {
-        return nextConfig.webpack(config, options)
-      }
+        if (typeof nextConfig.webpack === 'function') {
+          return nextConfig.webpack(config, options)
+        }
 
-      return config;
-    },
-  });
+        return config;
+      },
+    });
+  };
 }
